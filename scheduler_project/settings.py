1caff965613f518cc59b5601bedc4f6c60058b17
Noel Langat <noellangat28@gmail.com>
2023-05-19 10:08:16 +0300

    Update settings.py
    Update allowed hosts and disable debug

--- scheduler_project/settings.py
+++ scheduler_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d)$7okcb)xissz1b091vg0v$4=2drvj*$q2uc@pq7wht*-mz(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'scheduler.pythonanywhere.com']
 
 
 # Application definition
